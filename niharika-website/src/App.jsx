165ccdfbdf4bff68874e2ba76e7b0ef772f3e288
n-patil12 <nihapatil38@gmail.com>
2026-07-01 15:20:35 -0500

    started working on the home page

--- niharika-website/src/App.jsx
+++ niharika-website/src/App.jsx
@@ -1,11 +1,23 @@
+import React from 'react'
 import { useState } from 'react'
 import './App.css'
+import SomlaCoffee from './assets/SomlaCoffee.gif'
 
 function App() {
   
   return (
     <>
-    <h1>hello chingus lol hi bye</h1>
+    <div className='bg-[#c68f3a] w-screen h-screen'>
+      
+      <img src={SomlaCoffee} className='inline-2xl h-screen ml-30' />
+
+      <div>
+        <h1></h1>
+      </div>
+      
+      
+      {/* Rest of your app content goes here */}
+    </div>
     </>
   )
 }
---
+++ niharika-website/src/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import './Navbar.css'
+
+function Navbar() {
+    return (
+    <>
+    <nav className="navbar"> 
+        <div className="navbar-left">
+            <a>Niharika's hehe logo</a>
+        </div>
+        <div className="navbar-center">
+            <a>Niharika's Logo</a>
+        </div>
+        <div className="navbar-right">
+            <a>Niharika's Logo</a>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
